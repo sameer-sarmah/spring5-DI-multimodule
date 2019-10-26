693bcf740d636c34c13b332aa8aa3e4d3110b2ea
Sameer Sarmah <sameerparansarmah@gmail.com>
2019-10-26 14:39:16 +0530

    northwind sql data

---
+++ category.service.jta/src/main/java/category/service/jta/DefaultCategoryService.java
@@ -0,0 +1,50 @@
+package category.service.jta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import api.ICategoryProvider;
+import api.ICategoryService;
+import category.entity.Category;
+import category.entity.Product;
+
+@Component(value="jtaService")
+public class DefaultCategoryService implements ICategoryService {
+
+	List<ICategoryProvider> categoriesProvider;
+	
+	@Qualifier("beverage")
+	ICategoryProvider beverage;
+
+	public DefaultCategoryService(List<ICategoryProvider> categories,ICategoryProvider beverage) {
+		super();
+		this.categoriesProvider = categories;
+		this.beverage = beverage;
+	}
+
+	@Override
+	public List<Category> findAll() {
+		List<Category> categories = categoriesProvider.stream().map((provider) -> {
+			return provider.getCategory();
+		}).collect(Collectors.toList());
+		return categories;
+	}
+
+	@Override
+	public List<Product> findAllProductsByCategory(String categoryName) {
+		List<ICategoryProvider> categories = categoriesProvider.stream().filter((category) -> {
+			return category.getName().equalsIgnoreCase(categoryName);
+		}).collect(Collectors.toList());
+		List<Product> products = new ArrayList<>();
+		if (categories != null && !categories.isEmpty()) {
+			products = categories.get(0).getProducts();
+		}
+		return products;
+
+	}
+
+}
